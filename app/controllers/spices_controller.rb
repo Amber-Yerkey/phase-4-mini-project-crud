977d4d0afe1a8b97f34e120865cf133c649b7fd9
Amber Frantz <amber.k.frantz@mgail.com>
2022-08-21 11:27:36 -0400

    Completed Assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,37 @@
+class SpicesController < ApplicationController
+        # GET /spices all spices
+        def index
+            spices = Spice.all
+            render json: spices
+        end
+        
+        # POST /spices creates new spice
+        def create
+            spice = Spice.create(spice_params)
+            render json: spice, status: :created
+        end
+    
+        # PATCH /spices/:id
+        def update
+            spice = find_spice
+            spice.update(spice_params)
+            render json: spice
+        end
+    
+        # DELETE /spices/:id
+        def destroy
+            spice = find_spice
+            spice.destroy
+            head :no_content
+        end
+    
+        private
+    
+        def find_spice
+            Spice.find(params[:id])
+        end
+        
+        def spice_params
+            params.permit(:title, :image, :description, :notes, :rating)
+        end
+end
